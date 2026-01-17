795ef15d91924408a73eac657c2103a735b579a2
mari00kkk <masa.kostenko89@gmail.com>
2026-01-17 15:00:33 +0700

    заменила ручную реализацию списка на std::list в LRU кеше

--- lab1/include/LRU.hpp
+++ lab1/include/LRU.hpp
@@ -2,25 +2,25 @@
 #define LRU_HPP
 
 #include "ICache.hpp"
-#include<map>
+#include <list>
+#include <unordered_map>
 #include <cstddef>
 
-class LRUCache: public ICache{
+class LRUCache: public ICache {
 private:
-    struct Node;
-    class LinkedList;
-
     size_t capacity;
-    std::map<int, Node*> mapCache;
-    LinkedList* list;
+    //начало-последний использованный элемент
+    std::list<std::pair<int, int>> lruList;
+
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> mapCache;
+    
 public:
     LRUCache(size_t capacity);
     ~LRUCache();
-
+    
     int get(int key) override;
     void put(int key, int value) override;
     int operator[](int key) override;
-
 };
 
 #endif
--- lab1/src/LRU.cpp
+++ lab1/src/LRU.cpp
@@ -1,134 +1,52 @@
 #include "../include/LRU.hpp"
-#include <cstddef>
+#include <list>
+#include <unordered_map>
+#include <iostream>
 
-struct LRUCache::Node{
-    int key;
-    int value;
-    Node *prev;
-    Node *next;
-    
-    Node(int k, int v):key(k),value(v), prev(nullptr), next(nullptr){}
-
-};
-
-class LRUCache::LinkedList{
-    private:
-    Node *head;
-    Node *tail;
-    public:
-    LinkedList(): head(nullptr), tail(nullptr){}
-    Node* GetHead(){
-        return head;
+LRUCache::LRUCache(size_t cap): capacity(cap) {
+    if (capacity == 0) {
+        std::cout << "CACHE DISABLED" << std::endl;
     }
-    Node* GetTail(){
-        return tail;
-    }
-    Node* push_front(int key, int value){
-        Node *ptr = new Node(key, value);
-        ptr->next = head;
-
-        if (head!=nullptr) {
-            head->prev=ptr;
-        }
-        if(tail==nullptr){
-            tail=ptr;
-        }
-
-        head=ptr;
-
-        return ptr;
-    }
-
-    void pop_back(){
-        if(tail == nullptr)return;
-
-        Node *ptr = tail->prev;
-        if(ptr!=nullptr){
-            ptr->next = nullptr;
-        }
-        else head = nullptr;
-        delete tail;
-        tail = ptr;
-    }
-
-    void remove(Node* node){
-        Node *ptr = node;
-        if(ptr == nullptr) return;
-
-        if(ptr == head){
-            if(ptr->next)ptr->next->prev = nullptr;
-            head = ptr->next;
-        }
-        else if (ptr ==tail) {
-            ptr->prev->next=nullptr;
-            tail = ptr->prev;
-        } else{
-        Node *left = ptr->prev;
-        Node *right = ptr->next;
-        left->next = right;
-        right->prev = left;
-        }
-        delete ptr;
-    } 
-
-    void addToFront(Node* node){
-        node->next = head;
-        if(head != nullptr){
-            head->prev = node;  
-        }
-        if(tail == nullptr){
-            tail = node;
-        }
-        head = node;
-
-    }
-    void moveToFront(Node* node){
-        remove(node);
-        addToFront(node);
-    }
-};
-
-LRUCache::LRUCache(size_t cap): capacity(cap){
-    list = new LinkedList();
 }
 
-LRUCache::~LRUCache(){
-    delete list;
-}
-
-int LRUCache::get(int key){
+int LRUCache::get(int key) {
     if (capacity == 0) return -1;
-
-    auto iterator = mapCache.find(key);
-    if (iterator!=mapCache.end()){
-        list->moveToFront(iterator->second);
-        return iterator->second->value;
-    }
-    else {
+    
+    auto it = mapCache.find(key);
+    if (it == mapCache.end()) {
         return -1;
     }
-
+    
+    lruList.splice(lruList.begin(), lruList, it->second);
+    return it->second->second;
 }
- 
-void LRUCache::put(int key, int value){
+
+void LRUCache::put(int key, int value) {
     if (capacity == 0) return;
-
-    auto iterator = mapCache.find(key);
-    if(iterator != mapCache.end()){
-        mapCache[key]->value = value;
-        list->moveToFront(mapCache[key]);
-    }
-    else {
-         if(mapCache.size()>=capacity){
-            int old_key = list->GetTail()->key;
-            list->pop_back();
-            mapCache.erase(old_key);
+    
+    auto it = mapCache.find(key);
+    
+    if (it != mapCache.end()) {
+        // Ключ уже существует - обновляем значение и перемещаем в начало
+        it->second->second = value;
+        lruList.splice(lruList.begin(), lruList, it->second);
+    } else {
+        // Новый ключ
+        if (mapCache.size() >= capacity) {
+            auto last = lruList.end();
+            last--;
+            mapCache.erase(last->first);
+            lruList.pop_back();
         }
-        Node* new_node = list->push_front(key, value);
-        mapCache[key]= new_node;       
+        
+        lruList.push_front({key, value});
+        mapCache[key] = lruList.begin();
     }
 }
 
-int LRUCache::operator[](int key){
+int LRUCache::operator[](int key) {
     return get(key);
+}
+
+LRUCache::~LRUCache() {
 }
